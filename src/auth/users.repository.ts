626a834885ea1589e1be4327349809223e128ecd
Alisson Andrade <aqandrade23@gmail.com>
2021-12-19 19:43:29 -0300

    feat: validate duplicate username

--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -1,6 +1,12 @@
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { EntityRepository, Repository } from 'typeorm';
 import { AuthCredentialsDto } from './dto/auth-credential.dto';
 import { User } from './user.entity';
+
+const DUPLICADE_ERROR_CODE = '23505';
 
 @EntityRepository(User)
 export class UsersRepository extends Repository<User> {
@@ -8,6 +14,14 @@ export class UsersRepository extends Repository<User> {
     const { username, password } = authCredentialsDto;
 
     const user = this.create({ username, password });
-    await this.save(user);
+    try {
+      await this.save(user);
+    } catch (error) {
+      if (error.code === DUPLICADE_ERROR_CODE) {
+        throw new ConflictException('username already exists');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
   }
 }
