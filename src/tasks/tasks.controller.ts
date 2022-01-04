500b7d22cf1bd2bca64c787e6cf29044b5d9748a
Alisson Andrade <aqandrade23@gmail.com>
2022-01-04 19:56:59 -0300

    feat: get all tasks of owner User

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -25,8 +25,11 @@ export class TasksController {
   constructor(private tasksService: TasksService) {}
 
   @Get()
-  getTasks(@Body() tasksFilterDto: GetTasksFilterDto): Promise<Task[]> {
-    return this.tasksService.getTasks(tasksFilterDto);
+  getTasks(
+    @Body() tasksFilterDto: GetTasksFilterDto,
+    @GetUser() user: User,
+  ): Promise<Task[]> {
+    return this.tasksService.getTasks(tasksFilterDto, user);
   }
 
   @Get('/:id')
